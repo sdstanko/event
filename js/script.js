dc5918b0cac29dd33970b1e2f486d602504fdf92
Даниэль Станко <daniel@MacBook-Pro-Daniel.local>
2020-09-11 22:01:53 +0300

    intro section responsive made

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.reviews__container', {
+let mySwiper = new Swiper('.reviews__container', {
 	speed: 400,
 	spaceBetween: 50,
 	loop: false,
@@ -16,3 +16,28 @@ var mySwiper = new Swiper('.reviews__container', {
 	},
 
 })
+
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
+
+
+
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.nav');
+let login = document.querySelector('.login');
+
+burger.addEventListener('click', function(event) {
+	burger.classList.toggle('active');
+	nav.classList.toggle('active');
+	login.classList.toggle('active');
+	document.querySelector('html').classList.toggle('lock');
+});
